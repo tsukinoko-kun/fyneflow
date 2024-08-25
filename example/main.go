8c1a5c39554dfb425143fa803b58ef523ea00ced
Frank Mayer <gh@frankmayer.dev>
2024-08-25 12:45:36 +0200

    use int as key

--- example/main.go
+++ example/main.go
@@ -8,41 +8,42 @@ import (
 	"github.com/tsukinoko-kun/fyneflow"
 )
 
+const (
+	FlowKeyA = iota
+	FlowKeyB
+)
+
 func main() {
-	app := app.New()
-	window := app.NewWindow("Flow Test")
+	a := app.New()
+	w := a.NewWindow("Flow Test")
 
 	// wrap the fyne.Window using a flow
 	// the generic type is the type for the flow keys
-	flow := fyneflow.NewFlow(window)
+	flow := fyneflow.NewFlow(w)
 	defer flow.Close()
 
-	// add an item to the flow with the key "a"
-	flow.Set("a", func() fyne.CanvasObject {
+	// add an item to the flow with the key A
+	flow.Set(FlowKeyA, func() fyne.CanvasObject {
 		// use flow shared data of key "txt"
-		txt := flow.UseStateStr("txt", "test text")
 		return container.NewVBox(
 			widget.NewLabel("I am A"),
-			widget.NewEntryWithData(txt),
 			widget.NewButton("Go to B", func() {
-				// swich to flow item with key "b"
-				if err := flow.GoTo("b"); err != nil {
+				// swich to flow item with key B
+				if err := flow.GoTo(FlowKeyB); err != nil {
 					panic(err)
 				}
 			}),
 		)
 	})
 
-	// add an item to the flow with the key "b"
-	flow.Set("b", func() fyne.CanvasObject {
+	// add an item to the flow with the key B
+	flow.Set(FlowKeyB, func() fyne.CanvasObject {
 		// use flow shared data of key "txt"
-		txt := flow.UseStateStr("txt", "test text")
 		return container.NewVBox(
 			widget.NewLabel("I am B"),
-			widget.NewEntryWithData(txt),
 			widget.NewButton("Go to A", func() {
-				// swich to flow item with key "a"
-				if err := flow.GoTo("a"); err != nil {
+				// swich to flow item with key A
+				if err := flow.GoTo(FlowKeyA); err != nil {
 					panic(err)
 				}
 			}),
@@ -50,5 +51,5 @@ func main() {
 	})
 
 	// display the window
-	window.ShowAndRun()
+	w.ShowAndRun()
 }
--- main.go
+++ main.go
@@ -2,27 +2,36 @@ package fyneflow
 
 import (
 	"errors"
+	"math"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/data/binding"
 )
 
-type FlowItemGenerator func() fyne.CanvasObject
+type (
+	FlowKey int
 
-type FlowItem struct {
-	generator FlowItemGenerator
-}
+	FlowItemGenerator func() fyne.CanvasObject
 
-type Flow struct {
-	window  fyne.Window
-	current string
-	next    chan string
-	items   map[string]*FlowItem
+	FlowItem struct {
+		generator FlowItemGenerator
+	}
 
-	strState map[string]binding.String
-	intState map[string]binding.Int
-	close    bool
-}
+	Flow struct {
+		window  fyne.Window
+		current FlowKey
+		next    chan FlowKey
+		items   map[FlowKey]*FlowItem
+
+		strState map[string]binding.String
+		intState map[string]binding.Int
+		close    bool
+	}
+)
+
+const (
+	FlowKeyNone FlowKey = math.MinInt
+)
 
 // NewFlow creates a new Flow.
 // The generic type is the type for the flow keys.
@@ -31,9 +40,10 @@ type Flow struct {
 // Use the Close method to close the Flow. This will not close the associated fyne.Window!
 func NewFlow(w fyne.Window) *Flow {
 	f := new(Flow)
+	f.current = FlowKeyNone
 	f.window = w
-	f.items = make(map[string]*FlowItem)
-	f.next = make(chan string, 1)
+	f.items = make(map[FlowKey]*FlowItem)
+	f.next = make(chan FlowKey, 1)
 	go f.loop()
 	return f
 }
@@ -41,7 +51,7 @@ func NewFlow(w fyne.Window) *Flow {
 // Close closes the Flow.
 // This will not close the associated fyne.Window!
 func (f *Flow) Close() {
-	f.next <- ""
+	f.next <- FlowKeyNone
 
 	f.close = true
 	for k := range f.items {
@@ -60,7 +70,7 @@ func (f *Flow) loop() {
 
 // Set adds a FlowItem to the Flow.
 // If the key already exists, the FlowItem will be overwritten.
-func (f *Flow) Set(key string, generator FlowItemGenerator) *FlowItem {
+func (f *Flow) Set(key FlowKey, generator FlowItemGenerator) *FlowItem {
 	fi := new(FlowItem)
 	fi.generator = generator
 	apply := len(f.items) == 0
@@ -73,7 +83,7 @@ func (f *Flow) Set(key string, generator FlowItemGenerator) *FlowItem {
 
 // GoTo sets the content of the window to the content of the FlowItem associated with the given key.
 // If the key is not found, an error is returned.
-func (f *Flow) GoTo(key string) error {
+func (f *Flow) GoTo(key FlowKey) error {
 	if f.current == key {
 		return nil
 	}
@@ -86,7 +96,7 @@ func (f *Flow) GoTo(key string) error {
 	}
 }
 
-func (f *Flow) apply(next string) {
+func (f *Flow) apply(next FlowKey) {
 	fi, ok := f.items[next]
 	if !ok || fi == nil {
 		return
@@ -99,40 +109,6 @@ func (f *Flow) apply(next string) {
 }
 
 // Current returns the key of the FlowItem that is currently displayed.
-func (f *Flow) Current() string {
+func (f *Flow) Current() FlowKey {
 	return f.current
-}
-
-// UseStateStr returns a binding.String that is associated with the given key.
-// If the key does not exist, a new binding.String is created with the given default value.
-// If the key does exist, the default value is ignored.
-// The returned binding.String is shared between all FlowItems of the Flow.
-func (f *Flow) UseStateStr(key string, def string) binding.String {
-	if f.strState == nil {
-		f.strState = make(map[string]binding.String)
-	}
-
-	if _, ok := f.strState[key]; !ok {
-		f.strState[key] = binding.BindString(&def)
-		_ = f.strState[key].Set(def)
-	}
-
-	return f.strState[key]
-}
-
-// UseStateInt returns a binding.Int that is associated with the given key.
-// If the key does not exist, a new binding.Int is created with the given default value.
-// If the key does exist, the default value is ignored.
-// The returned binding.Int is shared between all FlowItems of the Flow.
-func (f *Flow) UseStateInt(key string, def int) binding.Int {
-	if f.intState == nil {
-		f.intState = make(map[string]binding.Int)
-	}
-
-	if _, ok := f.intState[key]; !ok {
-		f.intState[key] = binding.BindInt(&def)
-		_ = f.intState[key].Set(def)
-	}
-
-	return f.intState[key]
 }
